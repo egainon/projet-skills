3cc4eadab771bc986a464d0f41623b4ff672f6be
Emilie Gainon <emiliegainon@hotmail.fr>
2026-02-06 11:51:34 +0100

    Debut de back, routes

---
+++ Back/index.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+
+const express = require('express');
+const {neon} = require('@neondatabase/serverless');
+
+const app = express();
+const PORT = process.env.PORT || 4242;
+
+app.get('/', async (_, res) => {
+    try {
+    const sql = neon(`${process.env.DATABASE_URL}`);
+    const response = await sql`
+        SELECT
+            themes.id,
+            themes.libelle,
+            skills.id,
+            skills.intitule,
+            skills.niveau
+        FROM themes
+        LEFT JOIN skills ON skills.themes_id = themes.id
+        ORDER BY themes.id;
+        `;
+        console.log(response);
+        res.json(response);
+    } catch (error) {
+        console.error("Erreur SQL :", error);
+        res.status(500).json({ error: "Erreur serveur" });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening to http://localhost:${PORT}`);
+})
